8fbdd2da9f3acc99695c22460dc6174464088e14
Sonja <youssefkoko01@gmail.com>
2021-01-04 13:31:09 +0100

    highlight Event link in the nav bar if we are in the Past Event page

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
             <li>
               <a href="#">Programs</a>
             </li>
-            <li <?php if ( get_post_type() == 'event') echo 'class="current-menu-item"'; ?>>
+            <li <?php if ( get_post_type() == 'event' OR is_page('past-event')) echo 'class="current-menu-item"'; ?>>
               <a href="<?php echo get_post_type_archive_link('event');?>">Events</a>
             </li>
             <li>
