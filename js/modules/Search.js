37cf45d8994b4ebab0f40c1e6295df8462506029
Sonja <youssefkoko01@gmail.com>
2021-01-07 15:10:40 +0100

    Create function to display Spinner after Typing Letters and check if the value of searchField does NOT changed if so, then dont display The Spinner and if The field Cleared then DO NOT display anything inside HTML div.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -2,11 +2,14 @@ import $ from 'jquery';
 class Search {
   // Descripe or Initiate or Create the Object
   constructor(){
+    this.resultsDev = $('#search-overlay__results');
     this.openButton = $('.js-search-trigger');
     this.closeButton = $('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
     this.searchField = $('#search-term');
     this.typingTimer;
+    this.isSpinnerVisibale = false;
+    this.previousValue;
     this.events();
     this.isOverlayOpen = false;
   }
@@ -15,17 +18,33 @@ class Search {
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
     $(document).on('keydown', this.keyPressToggle.bind(this));
-    this.searchField.on('keydown', this.typingLogic.bind(this));
+    this.searchField.on('keyup', this.typingLogic.bind(this));
   }
   // 3. Methods (Functions, Action .. )
   typingLogic(){
-    clearTimeout(this.typingTimer);
-    this.typingTimer = setTimeout(function(){
-      console.log('hello from logic');
-    }, 2000);
+    // check if field does not change its value if so then the Spinner can work
+    if(this.searchField.val() !== this.previousValue){
+      clearTimeout(this.typingTimer);
+      if(this.searchField.val()){
+        if(!this.isSpinnerVisibale){
+          this.resultsDev.html('<div class="spinner-loader"></div>')
+          this.isSpinnerVisibale = true;
+        }    
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      }else{
+        this.resultsDev.html('');
+        this.isSpinnerVisibale = false;
+      }
+      // end if 
+    }
+    this.previousValue = this.searchField.val();
+  }
+  getResults(){
+    this.resultsDev.html('imagine reuslts here');
+    this.isSpinnerVisibale = false;
   }
   keyPressToggle(e){
-    if(e.keyCode == 83 && !this.isOverlayOpen){
+    if(e.keyCode == 83 && !this.isOverlayOpen && !$('input, textarea').is(':focus')){
       this.openOverlay();
     }
     if(e.keyCode == 27 &&  this.isOverlayOpen){
