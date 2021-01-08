6bab4cbf2a70ee4f7c2ef7cd4422a4ecb379536e
Sonja <youssefkoko01@gmail.com>
2021-01-08 23:30:21 +0100

    create new custom wp rest api

---
+++ inc/search-route.php
@@ -0,0 +1,24 @@
+<?php
+add_action('rest_api_init', 'university_register_search');
+
+function university_register_search(){
+  register_rest_route('university/v1', 'search', array(
+    'method' => WP_REST_SERVER::READABLE,
+    'callback' => 'universitySearchResults'
+  ));
+}
+
+function universitySearchResults(){
+  $professors = new WP_Query(array(
+    'post_type' => 'professor'
+  ));
+  $professorsResults = array();
+  while($professors->have_posts()){ 
+    $professors->the_post();
+    array_push($professorsResults, array(
+      'title' => get_the_title(),
+      'link' => get_the_permalink(),
+    ));
+  }
+  return $professorsResults;
+}
