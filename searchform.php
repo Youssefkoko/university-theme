1732a6a3cfd014b5c51c4746ea5b6c3ac5f1efb4
Sonja <youssefkoko01@gmail.com>
2021-01-11 01:29:55 +0100

    Created file for set HTML code of From in one place and display it in every page where we need search form by calling get_search_form().

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form class="search-form"  method="get" action="<?php echo esc_url(site_url('/')); ?>">
+      <label class="headline headline--medium" for="search-page"> Search What you looking for:</label>
+      <div class="search-form-row">
+        <input class="s" type="search" name="s" id="search-page">
+        <input class="submit" type="button" value="Search">
+      </div>
+          
+</form>
